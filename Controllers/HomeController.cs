3b73861f463b134a2194f75c78cc57fd28f44f03
tuananhowo <87203362+tuananhowo@users.noreply.github.com>
2023-05-11 11:25:31 +0700

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private webdoanvat db = new webdoanvat();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var m1 = db.Banners.FirstOrDefault(g => g.ThuTu == 1).LinkBanner;
+            var m2 = db.Banners.FirstOrDefault(g => g.ThuTu == 2).LinkBanner;
+            var m3 = db.Banners.FirstOrDefault(g => g.ThuTu == 3).LinkBanner;
+            if (m1 != null || m1 != null || m2 != null)
+            {
+                Session["Banner1"] = m1;
+                Session["Banner2"] = m2;
+                Session["Banner3"] = m3;
+            }
+            ViewData["hinhanh"] = db.Banners.AsNoTracking().Where(g => g.ThuTu == 1).ToList();
+            return View();
+        }
+        public PartialViewResult ListDanhMuc()
+        {
+            var danhmuc = db.DANHMUCs.Select(n => n);
+            return PartialView(danhmuc);
+        }
+        public PartialViewResult SanPhamMoi()
+        {
+            List<SANPHAM> sanphams = new List<SANPHAM>();
+
+            //sanphams = db.SANPHAMs.Select(h => h).ToList();
+            var sANPHAMs = db.SANPHAMs.OrderByDescending(h => h.ID_SP).Select(h => h).Take(6);
+
+            return PartialView(sANPHAMs);
+        }
+        public PartialViewResult TopGiamGia(string id)
+        {
+            List<SANPHAM> sanphams = new List<SANPHAM>();
+
+            var sANPHAMs = db.SANPHAMs.OrderByDescending(h => h.GiamGia).Select(h => h).Take(6);
+
+            return PartialView(sANPHAMs);
+        }
+        public ActionResult ListSanPham()
+        {
+            List<SANPHAM> sp = Session["SP"] as List<SANPHAM>;
+            var so = 0;
+            if (sp == null)
+            {
+                so = 0;
+            }
+            else
+            {
+                so = sp.Count;
+            }
+            ViewBag.sosp = so;
+            return View();
+        }
+        public PartialViewResult AllSanPham(string id, string keysearch = "")
+        {
+            List<SANPHAM> sanphams = new List<SANPHAM>();
+            if (id == null)
+            {
+                sanphams = db.SANPHAMs.Select(h => h).ToList();
+            }
+
+            else
+            {
+                sanphams = db.SANPHAMs.Where(h => h.ID_DanhMuc.ToString().Equals(id)).Select(h => h).ToList();
+                Session["SP"] = sanphams;
+            }
+            if (keysearch != "")
+            {
+                sanphams = db.SANPHAMs.Where(h => h.TenSanPham.ToLower().Contains(keysearch.ToLower())).ToList();
+            }
+            return PartialView(sanphams);
+        }
+        public PartialViewResult ShowListDanhMuc()
+        {
+            var danhmuc = db.DANHMUCs.Select(n => n);
+            return PartialView(danhmuc);
+        }
+        //chi tiết sản phẩm
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SANPHAM sanphams = db.SANPHAMs.SingleOrDefault(m => m.ID_SP == id);
+            if (sanphams == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sanphams);
+        }
+        public ActionResult XemChiTiet(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SANPHAM sp = db.SANPHAMs.SingleOrDefault(m => m.ID_SP == id);
+            if (sp == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sp);
+        }
+        //đăng nhập, đăng xuất
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string TenDangNhap, string MatKhau)
+        {
+            if (ModelState.IsValid)
+            {
+                var user1 = db.TAIKHOANs.FirstOrDefault(u => u.TenDangNhap.Equals(TenDangNhap) && u.MatKhau.Equals(MatKhau));
+                if (user1 != null)
+                {
+                    var newCookie = new HttpCookie("myCookie", user1.ID_TK.ToString());
+                    newCookie.Expires = DateTime.Now.AddDays(10);
+                    Response.AppendCookie(newCookie);
+                    if (user1.PhanQuyen == 3)
+                    {
+                        Session["HoTen"] = user1.HoTen;
+                        Session["Email"] = user1.Email;
+                        Session["ID_TK"] = user1.ID_TK;
+                        Session["HinhAnh"] = user1.HinhAnh;
+                        Session["PQ"] = user1.PhanQuyen;
+                        return Redirect("~/Home/Index");
+                    }
+                    else
+                    {
+                        ViewBag.error = "Thông tin đăng nhập không hợp lệ!!!";
+                    }
+                }
+
+                else
+                {
+                    ViewBag.error = "Thông tin đăng nhập không hợp lệ!!!";
+                }
+
+            }
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            if (Request.Cookies["myCookie"] != null)
+            {
+                //Fetch the Cookie using its Key.
+                HttpCookie nameCookie = Request.Cookies["myCookie"];
+
+                //Set the Expiry date to past date.
+                nameCookie.Expires = DateTime.Now.AddDays(-1);
+
+                //Update the Cookie in Browser.
+                Response.Cookies.Add(nameCookie);
+
+                //Set Message in TempData.
+                TempData["Message"] = "Cookie deleted.";
+            }
+            return RedirectToAction("Login");
+        }
+        public ActionResult BanDo()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register([Bind(Include = "ID_TK,TenDangNhap,MatKhau,HoTen,Email,NgaySinh,GioiTinh,DiaChi,TinhTrang,PhanQuyen,NgayTao,HinhAnh,SDT")] TAIKHOAN tAIKHOAN)
+        {
+            var m = Request["NgaySinh"];
+            tAIKHOAN.NgaySinh = DateTime.ParseExact(Request["NgaySinh"], "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            try
+            {
+
+
+                tAIKHOAN.HinhAnh = "";
+                tAIKHOAN.NgayTao = DateTime.Now;
+                var f = Request.Files["ImageFile"];
+                if (f != null && f.ContentLength > 0)
+                {
+                    //Use  Namespace  called  :	System.IO
+                    string FileName = System.IO.Path.GetFileName(f.FileName);
+                    //Lấy  tên  file  upload
+                    string UploadPath = Server.MapPath("~/wwwoot/dataimg/" + FileName);
+                    //Copy  Và  lưu  file  vào  server.
+                    f.SaveAs(UploadPath);
+                    //Lưu  tên  file  vào  trường
+                    tAIKHOAN.HinhAnh = FileName;
+                }
+                tAIKHOAN.TinhTrang = 1;
+                tAIKHOAN.GioiTinh = Request["GioiTinh"];
+                tAIKHOAN.PhanQuyen = 3;
+                db.TAIKHOANs.Add(tAIKHOAN);
+                db.SaveChanges();
+                //SetAlter("Thêm thành công", "success");
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = "Lỗi nhập dữ liệu" + ex.Message;
+
+                return View(tAIKHOAN);
+
+            }
+        }
+        public ActionResult ThongTinCN(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TAIKHOAN objTaiKhoan = db.TAIKHOANs.Find(id);
+            if (objTaiKhoan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(objTaiKhoan);
+        }
+        public ActionResult SuaTT(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TAIKHOAN objTaiKhoan = db.TAIKHOANs.Find(id);
+            if (objTaiKhoan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(objTaiKhoan);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SuaTT([Bind(Include = "ID_TK,TenDangNhap,MatKhau,HoTen,Email,SDT,NgaySinh,GioiTinh,DiaChi,TinhTrang,PhanQuyen,NgayTao,HinhAnh")] TAIKHOAN tAIKHOAN)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var f = Request.Files["ImageFile"];
+                    if (f != null && f.ContentLength > 0)
+                    {
+                        string FileName = System.IO.Path.GetFileName(f.FileName);
+                        string UploadPath = Server.MapPath("~/wwwoot/dataimg/" + FileName);
+                        Session["HinhAnh"] = FileName;
+                        f.SaveAs(UploadPath);
+                        tAIKHOAN.HinhAnh = FileName;
+                    }
+                    tAIKHOAN.TinhTrang = 1;
+                    tAIKHOAN.PhanQuyen = 3;
+                    tAIKHOAN.GioiTinh = Request["GioiTinh"];
+                    tAIKHOAN.NgaySinh = Convert.ToDateTime(Request["NgaySinh"]);
+                    db.TAIKHOANs.AddOrUpdate(tAIKHOAN);
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = "Nhập thông tin lỗi" + ex.Message;
+
+                return View(tAIKHOAN);
+            }
+        }
+
+
+    }
+}
---
+++ Controllers/ShoppingCartController.cs
@@ -0,0 +1,291 @@
+﻿using MoMo;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class ShoppingcartController : Controller
+    {
+        // GET: Shoppingcart
+        webdoanvat _db = new webdoanvat();
+        public Cart GetCart()
+        {
+            Cart cart = Session["Cart"] as Cart;
+            if (cart == null || Session["Cart"] == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+        //phuong thuc add item vao gio hang
+        public ActionResult AddtoCart(int id)
+        {
+            var pro = _db.SANPHAMs.SingleOrDefault(s => s.ID_SP == id);
+            GIOHANG a = new GIOHANG();
+            if (pro != null)
+            {
+                GetCart().Add(pro);
+                Cart cart = Session["Cart"] as Cart;
+                CartItem c = new CartItem();
+                a.ID_TK = int.Parse(Request.Cookies["myCookie"].Value);
+                a.SoTien = pro.GiaTien * 1;
+                a.DuLieu = pro.MoTa;
+                a.TinhTrang = 0;
+                a.ID_SP = pro.ID_SP;
+                a.ID_DanhMuc = pro.ID_DanhMuc;
+                a.SoLuong = 1;
+                _db.GIOHANGs.Add(a);
+                _db.SaveChanges();
+
+            }
+            var check = (string)Session["check"] ?? "";
+            check += a.ID_GioHang + ",";
+            Session["check"] = check;
+            return RedirectToAction("Index", "Home");
+
+        }
+        //trang gio hang
+        public ActionResult ShowToCart()
+        {
+            var r = Request.Cookies["myCookie"];
+            if (r != null)
+            {
+                ViewBag.coo = r.Value;
+            }
+            else
+            {
+                ViewBag.coo = "";
+            }
+            int tk = int.Parse(r.Value.ToString());
+            Session["ID_TK"] = r.Value;
+            var hoten = _db.TAIKHOANs.FirstOrDefault(g => g.ID_TK == tk).HoTen;
+            var hinhanh = _db.TAIKHOANs.FirstOrDefault(g => g.ID_TK == tk).HinhAnh;
+            ViewBag.HoTen = hoten;
+            ViewBag.HinhAnh = hinhanh;
+            if (Session["Cart"] == null)
+            {
+
+                return RedirectToAction("Index", "Home");
+
+            }
+            Cart cart = Session["Cart"] as Cart;
+            return View(cart);
+        }
+        public ActionResult Update_soluong(FormCollection form, int? ID_GioHang = 0)
+        {
+            Cart cart = Session["Cart"] as Cart;
+            int id_pro = int.Parse(form["ID_SP"]);
+            int Sluong = int.Parse(form["So_luong"]);
+            var tk = Convert.ToInt32(Request.Cookies["myCookie"].Value);
+            var list = _db.GIOHANGs.Where(g => g.ID_SP == id_pro && g.ID_TK == tk && g.TinhTrang == 0 && g.ID_GioHang == ID_GioHang).ToList();
+            foreach (var item in list)
+            {
+                var objSP = _db.SANPHAMs.FirstOrDefault(g => g.ID_SP == item.ID_SP);
+                if (objSP.SoLuong < Sluong && objSP.DANHMUC.KieuDM == 2)
+                {
+                    return RedirectToAction("ShowToCart", "ShoppingCart", new { sc = 1 });
+                }
+                else
+                {
+                    item.SoLuong = Sluong;
+                    _db.SaveChanges();
+                }
+            }
+            cart.UpdateSoluong(id_pro, Sluong);
+            return RedirectToAction("ShowToCart", "ShoppingCart");
+
+        }
+        //checkout
+        public ActionResult CheckOut()
+        {
+            var tk = Convert.ToInt32(Request.Cookies["myCookie"].Value);
+            var m = Session["check"].ToString();
+            var l = m.Trim(',').Split(',').ToList();
+            var List = _db.GIOHANGs.Where(g => g.TinhTrang == 0 && g.ID_TK == tk && l.Contains(g.ID_GioHang.ToString())).ToList();
+            Session["tt"] = List;
+            ViewBag.TK = _db.TAIKHOANs.FirstOrDefault(g => g.ID_TK == tk);
+
+            foreach (var item in List)
+            {
+                item.TinhTrang = 1;
+                _db.SaveChanges();
+            }
+            if (Session["Cart"] == null)
+            {
+
+                return RedirectToAction("CheckOut", "ShoppingCart");
+
+            }
+            Cart cart = Session["Cart"] as Cart;
+            return View(cart);
+        }
+        [HttpPost]
+        public ActionResult ThanhToan()
+        {
+
+            var tk = Convert.ToInt32(Request.Cookies["myCookie"].Value);
+            var k = _db.TAIKHOANs.FirstOrDefault(g => g.ID_TK == tk).MatKhau;
+            THANHTOAN ttt = new THANHTOAN();
+            ttt.ID_TK = int.Parse(Request.Cookies["myCookie"].Value);
+            ttt.PhuongThucTT = Request["tt"];
+            ttt.ThongTinTT = Request["tttt"];
+            ttt.BaoMat = k;
+            ttt.NgayTao = DateTime.Now;
+            ttt.TinhTrang = 1;
+            _db.THANHTOANs.Add(ttt);
+            _db.SaveChanges();
+            var m = Session["check"].ToString();
+            var l = m.Trim(',').Split(',').ToList();
+            var i = 0;
+            var list = Session["tt"] as List<GIOHANG> ?? new List<GIOHANG>();
+
+            Cart cart = Session["Cart"] as Cart;
+            Session["cart"] = new List<Cart>();
+            //return RedirectToAction("Index", "Home", new { sc = 1 });
+            string endpoint = "https://test-payment.momo.vn/gw_payment/transactionProcessor";
+            string partnerCode = "MOMOOJOI20210710";
+            string accessKey = "iPXneGmrJH0G8FOP";
+            string serectkey = "sFcbSGRSJjwGxwhhcEktCHWYUuTuPNDB";
+            string orderInfo = "Thanh toán hóa đơn";
+            string returnUrl = "https://localhost:44394/Home/ConfirmPaymentClient";
+            string notifyurl = "https://4c8d-2001-ee0-5045-50-58c1-b2ec-3123-740d.ap.ngrok.io/Home/SavePayment"; //lưu ý: notifyurl không được sử dụng localhost, có thể sử dụng ngrok để public localhost trong quá trình test
+            var tien = ttt.ThongTinTT.Replace("VND", "").Replace(".", "").Trim();
+            string amount = tien;
+            string orderid = DateTime.Now.Ticks.ToString(); //mã đơn hàng
+            string requestId = DateTime.Now.Ticks.ToString();
+            string extraData = "";
+
+            //Before sign HMAC SHA256 signature
+            string rawHash = "partnerCode=" +
+                partnerCode + "&accessKey=" +
+                accessKey + "&requestId=" +
+                requestId + "&amount=" +
+                amount + "&orderId=" +
+                orderid + "&orderInfo=" +
+                orderInfo + "&returnUrl=" +
+                returnUrl + "&notifyUrl=" +
+                notifyurl + "&extraData=" +
+                extraData;
+
+            MoMoSecurity crypto = new MoMoSecurity();
+            //sign signature SHA256
+            string signature = crypto.signSHA256(rawHash, serectkey);
+
+            //build body json request
+            JObject message = new JObject
+            {
+                { "partnerCode", partnerCode },
+                { "accessKey", accessKey },
+                { "requestId", requestId },
+                { "amount", amount },
+                { "orderId", orderid },
+                { "orderInfo", orderInfo },
+                { "returnUrl", returnUrl },
+                { "notifyUrl", notifyurl },
+                { "extraData", extraData },
+                { "requestType", "captureMoMoWallet" },
+                { "signature", signature }
+
+            };
+
+            string responseFromMomo = PaymentRequest.sendPaymentRequest(endpoint, message.ToString());
+
+        
+            if (ttt.PhuongThucTT == "1")
+            {
+                foreach (var item in list)
+                {
+                    //var id_gh = Convert.ToInt16(l[i]);
+                    item.TinhTrang = 2;
+                    item.ID_ThanhToan = ttt.ID_ThanhToan;
+                    item.NgayDatHang = DateTime.Now;
+                    _db.GIOHANGs.AddOrUpdate(item);
+                    // tru trong bang sp
+                    var objSP = _db.SANPHAMs.FirstOrDefault(g => g.ID_SP == item.ID_SP && g.DANHMUC.KieuDM == 2);
+                    if (objSP != null)
+                    {
+                        objSP.SoLuong -= item.SoLuong;
+                        _db.SANPHAMs.AddOrUpdate(objSP);
+                    }
+                    i++;
+
+                }
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Home", new { sc = 1 });
+            }
+            else
+            {
+                foreach (var item in list)
+                {
+                    //var id_gh = Convert.ToInt16(l[i]);
+                    item.TinhTrang = 3;
+                    item.ID_ThanhToan = ttt.ID_ThanhToan;
+                    item.NgayDatHang = DateTime.Now;
+                    _db.GIOHANGs.AddOrUpdate(item);
+                    // tru trong bang sp
+                    var objSP = _db.SANPHAMs.FirstOrDefault(g => g.ID_SP == item.ID_SP && g.DANHMUC.KieuDM == 2);
+                    if (objSP != null)
+                    {
+                        objSP.SoLuong -= item.SoLuong;
+                        _db.SANPHAMs.AddOrUpdate(objSP);
+                    }
+                    i++;
+
+                }
+                JObject jmessage = JObject.Parse(responseFromMomo);
+                _db.SaveChanges();
+                return Redirect(jmessage.GetValue("payUrl").ToString());
+            }
+
+        }
+        public ActionResult Xoa(int ID_SP = 0)
+        {
+            Cart cart = Session["Cart"] as Cart;
+            cart.Xoa(ID_SP);
+            return RedirectToAction("ShowToCart", "ShoppingCart");
+
+        }
+        public ActionResult ShowCart(int TinhTrang = -1)
+        {
+            var r = Request.Cookies["myCookie"];
+            if (r != null)
+            {
+                ViewBag.coo = r.Value;
+            }
+            else
+            {
+                ViewBag.coo = "";
+            }
+            int tk = int.Parse(r.Value.ToString());
+            Session["ID_TK"] = r.Value;
+            var hoten = _db.TAIKHOANs.FirstOrDefault(g => g.ID_TK == tk).HoTen;
+            var hinhanh = _db.TAIKHOANs.FirstOrDefault(g => g.ID_TK == tk).HinhAnh;
+            ViewBag.HoTen = hoten;
+            ViewBag.HinhAnh = hinhanh;
+            var query = _db.GIOHANGs.AsNoTracking().Where(g => g.ID_TK == tk);
+            if (TinhTrang > -1)
+            {
+                query = query.Where(g => g.TinhTrang == TinhTrang);
+            }
+            var list = query.ToList();
+            ViewData["sp"] = _db.SANPHAMs.AsNoTracking().ToList();
+            return View(list);
+        }
+        public ActionResult HuyDon(int ID_GioHang)
+        {
+            GIOHANG m = _db.GIOHANGs.FirstOrDefault(g => g.ID_GioHang == ID_GioHang);
+            m.TinhTrang = 4;
+            _db.GIOHANGs.AddOrUpdate(m);
+            _db.SaveChanges();
+            return RedirectToAction("ShowCart", "ShoppingCart");
+        }
+
+    }
+
+}
